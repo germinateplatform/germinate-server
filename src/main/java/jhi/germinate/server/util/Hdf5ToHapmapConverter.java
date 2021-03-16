d32b87a96c1b1e40b9c4603d0bc7f3851b877c39
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2021-03-16 18:32:35 +0000

    - CHG: Updated dependency libraries to fix an issue with HDF5 and a bundled version of Apache IO which was causing version conflicts.

--- src/main/java/jhi/germinate/server/util/Hdf5ToHapmapConverter.java
+++ src/main/java/jhi/germinate/server/util/Hdf5ToHapmapConverter.java
@@ -38,12 +38,12 @@ public class Hdf5ToHapmapConverter extends AbstractHdf5Converter
 			String[] stateTable = reader.readStringArray(STATE_TABLE);
 			System.out.println("Read statetable: " + (System.currentTimeMillis() - s) + " (ms)");
 
-			// Output any extra header lines that have been provided such as db link urls
+			// Output any extra header lines that have been provided
 			if (headerLines != null && !headerLines.isEmpty())
 				writer.print(headerLines);
 
 
-			// Write the header line of a Flapjack file
+			// Write the header line of a Hapmap file
 			writer.println(lines.stream().collect(Collectors.joining("\t", "rs#\talleles\tchrom\tpos\tstrand\tassembly#\tcenter\tprotLSID\tassayLSID\tpanelLSID\tQCcode\t", "")));
 
 			s = System.currentTimeMillis();
