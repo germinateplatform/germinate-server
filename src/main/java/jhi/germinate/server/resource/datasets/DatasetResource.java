b51269fdbc3af03452ab887608d27df4edb81278
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2021-11-09 09:28:59 +0000

    - FIX: Fixed an issue with HDF5 file creation where the source file doesn't contain any map information.

--- src/main/java/jhi/germinate/server/resource/datasets/DatasetResource.java
+++ src/main/java/jhi/germinate/server/resource/datasets/DatasetResource.java
@@ -109,6 +109,10 @@ public class DatasetResource extends ContextResource
 				{
 					case "genotype":
 						sourceFile = ResourceUtils.getFromExternal(ds.getSourceFile(), "data", "genotypes");
+						File transposed = new File(sourceFile.getParentFile(), "transposed-" + sourceFile.getName());
+
+						if (transposed.exists() && transposed.isFile())
+							transposed.delete();
 						break;
 					case "allelefreq":
 						sourceFile = ResourceUtils.getFromExternal(ds.getSourceFile(), "data", "allelefreq");
